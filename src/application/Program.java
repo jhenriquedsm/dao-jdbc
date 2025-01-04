b96b5509414f74bc4f3f6ac37fcb82ce4e596350
jhenriquedsm <jhsilvamata@gmail.com>
2025-01-04 15:03:52 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,5 +34,11 @@ public class Program {
         Seller newSeller = new Seller(null, "Frederico", "frederico@email.com", new Date(), 4000.0, department);
         sellerDAO.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("\n=== TEST 5: seller update ===");
+        seller = sellerDAO.findById(1);
+        seller.setName("Martha Wayne");
+        sellerDAO.update(seller);
+        System.out.println("Update Complete!");
     }
 }
